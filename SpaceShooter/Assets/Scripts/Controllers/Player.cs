4d3c316d9099cfde3ab5b15b27d20c28b6e40862
CalvinY28 <150267283+CalvinY28@users.noreply.github.com>
2024-09-18 11:27:47 -0400

    Player Controller with Velocity
    Added Method PLayerMovement
    Added Class notes

--- SpaceShooter/Assets/Scripts/Controllers/Player.cs
+++ SpaceShooter/Assets/Scripts/Controllers/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Codice.CM.Common;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,8 +10,56 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    //private Vector3 velocity = new Vector3(0.01f, 0f); // movement as variable
+    //private Vector3 velocity = Vector3.zero;
+    //private float speed = 0.001f;
+
+    public float playerSpeed = 0.01f; // for task 1
+
     void Update()
     {
+        //transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y); // moving x 0.01 every update
+        // transform.position += Vector3.right * 0.01f; // <--- this also works
+        //transform.position += velocity; // <--- this too
+
+        //velocity = Vector3.zero;
+        //transform.position += velocity.normalized * speed * Time.deltaTime; // <--- NORMALIZE TO MAKE VECTOR CONSISTENT AND TIME.DELTATIME TO KEEP FRAMERATE SAME
+
+        //velocity += acceleration * transform.up * Time.deltaTime; // Delta time needed on both (eg. 5fps vs 500)
+        //transform.position += velocity * Time.deltaTime;
+
+        //////////////////////////  Tasks  ////////////////////////////////////////////////////////////////////////////////////
+
+        PlayerMovement(playerSpeed); // I tried plugging in a vector3 on accident later realized i needed a float
+
+    }
+
+    public void PlayerMovement(float movement) // player controller method
+    {
+        //if (transform.position.x > 24) bonus
+        if (Input.GetKey(KeyCode.UpArrow)) // forgot i needed input to getting a key. originally just did "GetKey"
+        {
+            Vector3 velocity = new Vector3(0f, movement); // x y           y movement = up         -movement = down                 x movement = right       -movement = left
+            transform.position += velocity;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 velocity = new Vector3(0f, -movement);
+            transform.position += velocity;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 velocity = new Vector3(movement, 0f);
+            transform.position += velocity;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 velocity = new Vector3(-movement, 0f);
+            transform.position += velocity;
+        }
 
     }
 
