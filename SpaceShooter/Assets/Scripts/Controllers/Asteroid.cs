96741a6b2aaba33562e4c9727fd5c4579c2c4f98
CalvinY28 <calvinoct28@gmail.com>
2024-09-23 22:32:00 -0400

    Task 3 Complete: Moving Aestroids
    Moved the asteroids randomly

--- SpaceShooter/Assets/Scripts/Controllers/Asteroid.cs
+++ SpaceShooter/Assets/Scripts/Controllers/Asteroid.cs
@@ -5,16 +5,39 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour
 {
     public float moveSpeed;
-    public float arrivalDistance;
+    public float arrivalDistance; // use this to create a new position when position is reached
     public float maxFloatDistance;
+    //public float minFloatDistance;
+    public Vector3 targetPosition;
 
-    // Start is called before the first frame update
     void Start()
     {
+        NewPosition();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        MoveTo();
+    }
+
+    public void NewPosition()
+    {
+        //targetPosition = new Vector3(Random.Range(-maxFloatDistance, maxFloatDistance), Random.Range(-maxFloatDistance, maxFloatDistance), transform.position.z);
+
+        float randomX = Random.Range(-maxFloatDistance, maxFloatDistance);
+        float randomY = Random.Range(-maxFloatDistance, maxFloatDistance);
+
+        targetPosition = transform.position + new Vector3(randomX, randomY, 0);
+    }
+
+    public void MoveTo()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        //if (transform.position == targetPosition) // i dont even need arrival distance but i can use it anyway
+        if (Vector3.Distance(transform.position, targetPosition) < arrivalDistance )
+        {
+            NewPosition();
+        }
     }
 }
