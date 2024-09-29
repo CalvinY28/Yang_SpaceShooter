a55bdf00e3a83f458f614bc485412192f6c9a93b
CalvinY28 <calvinoct28@gmail.com>
2024-09-28 21:28:52 -0400

    Week 4 Task 3: Complete
    Added Orbital System

--- SpaceShooter/Assets/Scripts/Controllers/Moon.cs
+++ SpaceShooter/Assets/Scripts/Controllers/Moon.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
 
-    // Start is called before the first frame update
+    public float angle; // task 3 i can make this public to check my angles... it seems like the angle keeps going higher and higher. Might be able to clamp it and loop but im lazy.
+    public float speed; // task 3
+    public float radius; // task 3
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        OrbitalMotion(radius, speed, planetTransform);
     }
+
+    public void OrbitalMotion(float radius, float speed, Transform target)
+    {
+        float xPos = target.position.x + Mathf.Cos(angle) * radius; // left and right
+        float yPos = target.position.y + Mathf.Sin(angle) * radius; // up and down
+
+        angle += speed * Time.deltaTime;
+
+        transform.position = new Vector3(xPos, yPos, 0f);
+    }
+
 }
