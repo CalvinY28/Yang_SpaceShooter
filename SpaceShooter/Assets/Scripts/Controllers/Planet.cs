29c81b4c82dcfbd9d8570b73e077d4a043e46669
CalvinY28 <calvinoct28@gmail.com>
2024-10-13 22:10:06 -0400

    Mech 1: Session 3: Mechanic Completed
    Successfully made a list of things that can be pulled

--- SpaceShooter/Assets/Scripts/Controllers/Planet.cs
+++ SpaceShooter/Assets/Scripts/Controllers/Planet.cs
@@ -7,44 +7,53 @@ public class Planet : MonoBehaviour
     public float pullRadius = 10f;
     public float pullStrength = 5f;
 
-    public Transform player;
+    //public Transform player;
 
     public float orbitDistance = 1.5f;
     public float orbitSpeed = 50f;
-    private bool isOrbiting = false;
-    public float orbitAngle;
+    //private bool isOrbiting = false;
+    //public float orbitAngle;
+
+    public List<Transform> objectsToPullList;
+    private List<bool> isOrbitingList;
+    private List<float> orbitAnglesList;
 
     void Start()
     {
-        
+        isOrbitingList = new List<bool>(new bool[objectsToPullList.Count]);
+        orbitAnglesList = new List<float>(new float[objectsToPullList.Count]);
     }
 
     void Update()
     {
-        if (!isOrbiting)
+        for (int i = 0; i < objectsToPullList.Count; i++)
         {
-            ApplyGravitationalPull();
-        }
-        else
-        {
-            InOrbit();
+            if (!isOrbitingList[i])
+            {
+                ApplyGravitationalPull(i);
+            }
+            else
+            {
+                InOrbit(i);
+            }
         }
 
-        if (isOrbiting == false)
-        {
-            Debug.Log("is orbirting is false");
-        }
-        else
-        {
-            Debug.Log("is orbiting is true");
-        }
+        //if (isOrbiting == false)
+        //{
+        //    Debug.Log("is orbirting is false");
+        //}
+        //else
+        //{
+        //    Debug.Log("is orbiting is true");
+        //}
 
     }
 
-    void ApplyGravitationalPull()
+    void ApplyGravitationalPull(int index)
     {
         // looked at past lectures and work
-        Vector3 directionToPlanet = transform.position - player.position; // Distance between the planet and the player
+        Transform obj = objectsToPullList[index];
+        Vector3 directionToPlanet = transform.position - obj.position; // Distance between the planet and the player
         float distanceToPlanet = directionToPlanet.magnitude;
     
         // If the player is in pull radius then get pulled
@@ -55,41 +64,41 @@ public class Planet : MonoBehaviour
             float gravitationalForce = Mathf.Lerp(pullStrength, 0f, distanceToPlanet / pullRadius); // force with respect to distance
 
             Vector3 pullVector = directionToPlanet * gravitationalForce * Time.deltaTime;
-            player.position += pullVector;
+            obj.position += pullVector;
 
             if (distanceToPlanet < orbitDistance)
             {
-                isOrbiting = true;
+                isOrbitingList[index] = true;
 
                 // checking to make sure it orbits in the right spot
-                Vector3 check = player.position - transform.position;
-                orbitAngle = Mathf.Atan2(check.y, check.x);
+                Vector3 check = obj.position - transform.position;
+                orbitAnglesList[index] = Mathf.Atan2(check.y, check.x);
             }
 
         }
         else
         {
-            isOrbiting = false;
+            isOrbitingList[index] = false;
         }
     }
 
-    void InOrbit()
+    void InOrbit(int index)
     {
-        orbitAngle += orbitSpeed * Time.deltaTime * Mathf.Deg2Rad;  // multiply by deg2rad
+        orbitAnglesList[index] += orbitSpeed * Time.deltaTime * Mathf.Deg2Rad;  // multiply by deg2rad
 
-        float x = transform.position.x + Mathf.Cos(orbitAngle) * orbitDistance;
-        float y = transform.position.y + Mathf.Sin(orbitAngle) * orbitDistance;
+        float x = transform.position.x + Mathf.Cos(orbitAnglesList[index]) * orbitDistance;
+        float y = transform.position.y + Mathf.Sin(orbitAnglesList[index]) * orbitDistance;
 
-        player.position = new Vector3(x, y, 0f);
+        objectsToPullList[index].position = new Vector3(x, y, 0f);
 
         // make sure it orbits in the right spot this is wrong
         //player.position = (player.position - transform.position).normalized * orbitDistance + transform.position;
 
         // stopping the player from constantly orbiting
-        float currentDistanceToPlanet = Vector3.Distance(player.position, transform.position);
+        float currentDistanceToPlanet = Vector3.Distance(objectsToPullList[index].position, transform.position);
         if (currentDistanceToPlanet > orbitDistance)
         {
-            isOrbiting = false;
+            isOrbitingList[index] = false;
         }
 
     }
